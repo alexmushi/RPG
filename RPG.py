fb081959ed8325036c2797e633bf9f5b3316b378
alexmushi <111137234+alexmushi@users.noreply.github.com>
2022-10-07 10:12:16 -0500

    Add files via upload

--- RPG.py
+++ RPG.py
@@ -1,13 +1,6 @@
 #liberias
 from random import *
 
-global hp
-global atk
-global mana
-global curacion
-global pociones
-global xp
-
 """
 ==========Preguntas==========
 """
@@ -961,7 +954,9 @@ HPCaballero = 100
 AtkCaballero = 10
 ManaCaballero = 50
 CuracionCaballero = 1
-PocionesCaballero = 1
+pociones_m_c = ["Pocion_m"]
+pociones_d_c = ["Pocion_d"]
+InvCaballero = [pociones_m_c, pociones_d_c]
 
 #Alquimista
 
@@ -969,7 +964,9 @@ HPAlqui = 60
 AtkAlqui = 5
 ManaAlqui = 100
 CuracionAlqui = 1.5
-PocionesAlqui = 5
+pociones_m_a = ["Pocion_m", "Pocion_m", "Pocion_m"]
+pociones_d_a = ["Pocion_d", "Pocion_d", "Pocion_d"]
+InvAlqui = [pociones_m_a, pociones_d_a]
 
 #Tanque
 
@@ -977,7 +974,9 @@ HPTanque = 150
 AtkTanque = 15
 ManaTanque = 10
 CuracionTanque = 0.5
-PocionesTanque = 0
+pociones_m_t = []
+pociones_d_t = []
+InvTanque = [pociones_m_t, pociones_d_t]
 
 #Curandero
 
@@ -985,7 +984,9 @@ HPCurandero = 75
 AtkCurandero = 7
 ManaCurandero = 125
 CuracionCurandero = 2
-PocionesCurandero = 3
+pociones_m_cr = ["Pocion_m", "Pocion_m", "Pocion_m", "Pocion_m", "Pocion_m", "Pocion_m"]
+pociones_d_cr = []
+InvCurandero = [pociones_m_cr, pociones_d_cr]
 
 #Random
 
@@ -993,7 +994,19 @@ HPRandom = randrange(20, 200, 10)
 AtkRandom = randrange(2, 20)
 ManaRandom = randrange(2, 200, 10)
 CuracionRandom = randrange(1,3)
-PocionesRandom = randrange(0,7)
+PocionesMRandom = randrange(0,7)
+PocionesDRandom = randrange(0,7)
+pociones_m_r = []
+pociones_d_r = []
+while PocionesMRandom > 0:
+    pociones_m_r.append("Pocion_m")
+    PocionesMRandom -= 1
+while PocionesDRandom > 0:
+    pociones_d_r.append("Pocion_d")
+    PocionesDRandom -= 1
+InvRandom = [pociones_m_r, pociones_d_r]
+    
+
 
 """
 ==========Elegir clase==========
@@ -1006,27 +1019,36 @@ Caballero (C):\n
 HP = 100\n
 Atk = 10\n
 Mana = 50\n
-Pociones = 1\n
+Pociones de maná = 1\n
+Pociones de daño = 1\n
 \n
 Alquimista (A):\n
 HP = 60\n
 Atk = 5\n
 Mana = 100\n
-Pociones = 5\n
+Pociones de maná = 3\n
+Pociones de daño = 3\n
 \n
 Tanque (T):\n
 HP = 150\n
 Atk = 15\n
 Mana = 10\n
-Pociones = 0\n  
+Pociones de maná = 0\n
+Pociones de daño = 0\n 
 \n
 Curandero (CR)\n
 HP = 75\n
 Atk = 7\n
 Mana = 125\n
-Pociones = 3\n
+Pociones de maná = 6\n
+Pociones de daño = 0\n
 \n
 Random (R):\n
+HP = ???\n
+Atk = ???\n
+Mana = ???\n
+Pociones de maná = ???\n
+Pociones de daño = ???\n
 ''')
 
 clase = str(input("Escribe la inicial de tu clase "))
@@ -1037,42 +1059,48 @@ if clase == "C":
     atk = AtkCaballero
     mana = ManaCaballero
     curacion = CuracionCaballero
-    pociones = PocionesCaballero
+    pociones_m = pociones_m_c
+    pociones_d = pociones_d_c
+    inv = InvCaballero
 #alquimista
 elif clase == "A":
     hp = HPAlqui
     atk = AtkAlqui
     mana = ManaAlqui
     curacion = CuracionAlqui
-    pociones = PocionesAlqui
+    pociones_m = pociones_m_a
+    pociones_d = pociones_d_a
+    inv = InvAlqui
 #tanque
 elif clase == "T":
     hp = HPTanque
     atk = AtkTanque
     mana = ManaTanque
     curacion = CuracionTanque
-    pociones = PocionesTanque
+    pociones_m = pociones_m_t
+    pociones_d = pociones_d_t
+    inv = InvTanque
 #curandero
 elif clase == "CR":
     hp = HPCurandero
     atk = AtkCurandero
     mana = ManaCurandero
     curacion = CuracionCurandero
-    pociones = PocionesCurandero
+    pociones_m = pociones_m_cr
+    pociones_d = pociones_d_cr
+    inv = InvCurandero
 #random
 elif clase == "R":
     hp = HPRandom
     atk = AtkRandom
     mana = ManaRandom
     curacion = CuracionRandom
-    pociones = PocionesRandom
+    pociones_m = pociones_m_r
+    pociones_d = pociones_d_r
+    inv = InvRandom
 
-"""
-==========Overworld==========
-"""
-
-
-
+#nombre
+nombre = str(input("Escribe el nombre del guerrero: "))
 
 """
 ==========Stats boss==========
@@ -1085,11 +1113,20 @@ AtkBoss = 10
 ==========Pelea==========
 """
 
-print(" HP Jugador: ", hp, "\n HP Boss: ", HPBoss, "\n Mana: ", mana, "\n Pociones: ", pociones)
+print(" HP ", nombre, ": ", hp, "\n HP Boss: ", HPBoss, "\n Mana: ", mana)
+
+#Menu
 
 while HPBoss > 0 and hp > 0:
-    accion = input("Ataque = A, Curar = C, Pocion = P ")
-    
+
+    accion = input("Ataque = A, Curar = C, Pocion mana = PM, Pocion daño = PD, Inventario = I ")
+
+#inventario
+
+    while accion == "I" or accion == "i":
+        print("Tu inventario: " , inv)
+        accion = input("Ataque = A, Curar = C, Pocion mana = PM, Pocion daño = PD, Inventario = I ")
+        
 #ataque
 
     if accion == "A" or accion == "a":
@@ -1105,21 +1142,33 @@ while HPBoss > 0 and hp > 0:
         else:
             print("¡Necesitas mana para curarte!")
 
-#pocion
+#pocion mana
 
-    elif accion == "P" or accion == "p":
-        if pociones >= 1:
+    elif accion == "PM" or accion == "pm":
+        if "Pocion_m" in pociones_m:
             mana = mana + 20
-            pociones = pociones - 1
-        elif pociones == 0:
+            pociones_m.remove("Pocion_m")
+            print("Te regeneraste 20 mana")
+        else:
             print("¡Ya no te quedan pociones!")
+
+#pocion daño
+
+    elif accion == "PD" or accion == "pd":
+        if "Pocion_d" in pociones_d:
+            HPBoss = HPBoss - 20
+            pociones_d.remove("Pocion_d")
+            print("Le hiciste 20 daño al boss")
+        else:
+            print("¡Ya no te quedan pociones!")
+
 
 #daño boss
 
     dañoboss = randrange(1, 20)
     print("¡El boss te hizo ", dañoboss, " de daño!")
     hp = hp - dañoboss
-    print(" HP Jugador: ", hp, "\n HP Boss: ", HPBoss, "\n Mana: ", mana, "\n Pociones: ", pociones)
+    print(" HP ", nombre, ": ", hp, "\n HP Boss: ", HPBoss, "\n Mana: ", mana)
 
 
 #fin del combate
